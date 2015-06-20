1d42b7be03b22a2656f8a7cc19320af8db36752d
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2015-06-20 17:28:59 +0200

    feat(index.js): merge options

--- ngx/_build/index.js
+++ ngx/_build/index.js
@@ -2,6 +2,7 @@ import angular from "angular";
 import config from "./config";
 
 export default function(ngModule, options){
+  angular.merge(config, options);
   ngModule.run(registerErrorHandler);
 };
 
